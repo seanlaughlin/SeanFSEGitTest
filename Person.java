6a4447e8ca834dc1459a7b3e9a680d0efe8e8819
= <=>
2020-11-19 13:52:20 +0000

    Person class getters and setters second commit

--- Person.java
+++ Person.java
@@ -16,4 +16,20 @@ private String familyName;
 	this.firstName = firstName;
 	this.familyName = familyName;
 	}
+
+	public String getFirstName(){
+	return firstName;
+	}
+
+	public void setFirstName(String firstName){
+	this.firstName = firstName;
+	}
+
+	public String getFamilyName(){
+	return familyName;
+	}
+
+	public void setFamilyName(String familyName){
+	this.familyName = familyName;
+	}
 }
